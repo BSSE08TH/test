61dafb81b9831225ed0ec1e0e63d91ba6804e5d7
Shaishab <bsse0815@iit.du.ac.bd>
2019-10-15 13:23:48 +0600

    Change in master loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,10 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello test");
     System.out.println("Change in master");
-    int a = 108;
+    int a = 345;
     TestClass cls = new TestClass();
-    cls.print(a+100);
+    for (int i = 0; i < 5; i++) {
+      cls.print(a + 100);
+    }
   }
 }

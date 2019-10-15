9866c2ca8eabcf76f5f63f15de9aff0b1e92d171
Shaishab <bsse0815@iit.du.ac.bd>
2019-10-15 13:32:13 +0600

    remove test loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,5 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello test");
     System.out.println("Change in master");
-
-    int a = 345;
-    TestClass cls = new TestClass();
-    for (int i = 0; i < 5; i++) {
-      cls.print(a + 100);
-    }
   }
 }

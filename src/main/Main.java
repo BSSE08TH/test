56f95dbcc4d526b5a017d46b14c14dc89a3fe7cd
Shaishab <bsse0815@iit.du.ac.bd>
2019-10-15 13:24:52 +0600

    Remove all

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,5 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello test");
     System.out.println("Change in master");
-    int a = 108;
-    TestClass cls = new TestClass();
-    cls.print(a+100);
   }
 }
